54d7e9937250d4b46f5ffcad345382c0945536ff
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-04 23:33:26 +0400

    added body-parser

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dns = require("dns");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({optionsSuccessStatus:200}));
 
 app.use("/public",express.static(__dirname+"/public"));
-app.use(cors({optionsSuccessStatus:200}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// var w3 = dns.lookup('www.w3schools.com', function (err, addresses, family) {
+    //     console.log(addresses);
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
 
-const port = process.env.PORT || 3000;
+
+
 
 app.listen(port,()=>{
     console.log("App started at port: "+port);
