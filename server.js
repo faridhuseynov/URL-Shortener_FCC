a2ac14801f516bbfb257cabc6b0157cc508a200b
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-04 23:22:18 +0400

    cors added to the server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use("/public",express.static(__dirname+"/public"));
+app.use(cors({optionsSuccessStatus:200}));
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
