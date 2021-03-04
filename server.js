a97c260edf0ef5ebef359c497321861e28ae272d
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-05 00:37:24 +0400

    added check for the previous submission of the url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dns = require("dns");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+var websites={"1":"https://www.facebook.com"};
 app.use(cors({optionsSuccessStatus:200}));
 
 app.use("/public",express.static(__dirname+"/public"));
@@ -18,8 +18,29 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
 
+app.get("/api/shortulr/:short",(req,res)=>{
+    var link = req.params;
+    console.log(link);
+})
 
-
+app.post('/api/shorturl/new',(req,res)=>{
+    const url= req.body.website;
+        dns.lookup(String(url),(err,addresses,family)=>{
+        console.log(err);
+        if(err){
+            res.json({"error":"Invalid Hostname"});
+        }else{
+            for(const [key,value]of Object.entries(websites)){
+                if(value==String(url)){
+                    return res.json({"original_url":url,"short_url":key});
+                }
+            }
+            var next = Object.keys(websites).length+1;
+            websites[next]=url;
+            res.json({"original_url":url,"short_url":next});
+        }
+    })
+})
 
 app.listen(port,()=>{
     console.log("App started at port: "+port);
